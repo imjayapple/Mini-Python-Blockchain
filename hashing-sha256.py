ea73a37ceab0ab444484f4012d2e01b6e5388d04
imjayapple <imjayapple@gmail.com>
2023-05-23 21:59:38 -0700

    Add the option for a user to input text for this program to hash

--- hashing-sha256.py
+++ hashing-sha256.py
@@ -1,9 +1,9 @@
 # import sha256
 from hashlib import sha256
 # text to hash
-text = "I am excited to learn about blockchain"
+text = input("Enter the text to hash:")
 hash_result = sha256(text.encode())
 
 # print result
-print(hash_result.hexdigest())
+print("Hash:", hash_result.hexdigest())
 
