cfa69ec5bfae663f0ca83b7321f094bc55cace95
imjayapple <imjayapple@gmail.com>
2023-05-31 23:27:16 -0700

    My own simple blockchain, simple-blockchain.py

---
+++ simple-blockchain.py
@@ -0,0 +1,16 @@
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self, data, previous_hash):
+        self.timestamp = datetime.datetime.now()
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        hash_data = str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        return hashlib.sha256(hash_data.encode()).hexdigest()
+    
+class Blockchain:
+    
