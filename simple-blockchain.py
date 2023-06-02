73be44eff9648d78dd164c04cd917811799b75ad
imjayapple <imjayapple@gmail.com>
2023-06-01 21:30:15 -0700

    Blockchain class complete

--- simple-blockchain.py
+++ simple-blockchain.py
@@ -13,3 +13,48 @@ class Block:
         return hashlib.sha256(hash_data.encode()).hexdigest()
     
 class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block("Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, new_block):
+        new_block.previous_hash = self.get_latest_block().hash
+        new_block.hash = new_block.calculate_hash()
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i-1]
+
+            if current_block.hash != current_block.calculate_hash():
+                return False
+
+            if current_block.previous_hash != previous_block.hash:
+                return False
+
+        return True
+
+# Example usage
+blockchain = Blockchain()
+
+# Adding blocks to the blockchain
+blockchain.add_block(Block("Block 1 Data", ""))
+blockchain.add_block(Block("Block 2 Data", ""))
+blockchain.add_block(Block("Block 3 Data", ""))
+
+# Printing the blockchain
+for block in blockchain.chain:
+    print("Timestamp:", block.timestamp)
+    print("Data:", block.data)
+    print("Hash:", block.hash)
+    print("Previous Hash:", block.previous_hash)
+    print("-------------------------------------")
+
+# Validating the blockchain
+print("Is Blockchain Valid?", blockchain.is_chain_valid())
